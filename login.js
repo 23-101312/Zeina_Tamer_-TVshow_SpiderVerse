03e9c7b7e91ede742b0cab55db4b4687301b09fd
zeina <23-101312@students.eui.edu.eg>
2025-12-09 22:29:05 +0200

    login_to_java_all

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+document.getElementById("style").innerHTML = `Your Credentials are Incorrect`
+
 function test(){
   let gmail = document.getElementById("gmail").value;
   let password = document.getElementById("password").value;
@@ -13,3 +15,9 @@ function test(){
   }
   }
 }
+
+document.getElementById("login_title").innerHTML = `WELCOME BACK`
+
+document.getElementById("login_subtitle").innerHTML = ` Enter your universe credentials`
+
+document.getElementById("login_btn").innerHTML = `  ENTER SPIDER-VERSE`
